1045d16342af5f92514b99a3b166c5af670879ed
malihua <13683341906@163.com>
2017-02-16 13:15:58 +0800

    K24健身房项目

---
+++ K24/js/index.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+(function(win,doc){
+		function change(){
+		doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/320+'px';
+		}
+		change();
+		win.addEventListener('resize',change,false)
+	})(window,document);
